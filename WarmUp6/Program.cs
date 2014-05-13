ead51eda12d1834967153ff218e9d4251ea5af9e
Youngs285 <nitronite500@gmail.com>
2014-05-13 09:22:10 -0400

    Unfinished

---
+++ WarmUp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarmUp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter A Number: ");
+
+        }
+    }
+}
